a5f0ec48acbd3ac543e1688ce6afd8f4eb5688ee
Bruno Trecenti <bruno.lopestrecenti@gmail.com>
2020-08-12 16:57:00 -0300

    Update homebrew installer

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -10,7 +10,7 @@ if test ! $(which brew)
 then
   echo "Installing Homebrew."
 
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 exit 0
