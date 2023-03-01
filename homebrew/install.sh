697281893d909857b78b7e2976d5cc984c0bccca
Bruno Trecenti <bruno.lopestrecenti@gmail.com>
2023-02-28 22:44:38 -0300

    Updade homebrew install curl

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -10,7 +10,7 @@ if test ! $(which brew)
 then
   echo "Installing Homebrew."
 
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 exit 0
