e052aef2c09cc9dba16de2ab101f5270444c707f
Bruno Trecenti <bruno.lopestrecenti@gmail.com>
2025-09-03 09:39:31 -0300

    Sep 2025 update

--- vim/setup.sh
+++ vim/setup.sh
@@ -3,8 +3,9 @@
 # VIM
 #
 # This will setup vim configurations
-
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ ! -d ~/.vim/bundle/Vundle.vim ] ; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 cp ./vimrc ~/.vimrc
 vim +PluginInstall +qall
 echo "colorscheme molokai" >> ~/.vimrc
